72944ee759d3fb4367a98eed968d13e830f8d7f8
Kunal Kapoor <kunalkapoor.com@gmail.com>
2016-11-22 09:08:09 -0500

    Comments to rem

--- scripts/minikube-windows/startup.bat
+++ scripts/minikube-windows/startup.bat
@@ -1,11 +1,9 @@
-##########################################################################
-# This script contains the initial steps required to install and configure
-# Minikube and its command line client kubectl
-##########################################################################
+rem This script contains the initial steps required to install and configure
+rem Minikube and its command line client kubectl
 
-# Install Minikube from https://github.com/kubernetes/minikube/releases
+rem Install Minikube from https://github.com/kubernetes/minikube/releases
 
-# Install Kubectl from http://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/windows/amd64/kubectl.exe
+rem Install Kubectl from http://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/windows/amd64/kubectl.exe
 
-# Create a local Kubernetes cluster with one node using Minikube
+rem Create a local Kubernetes cluster with one node using Minikube
 minikube start
