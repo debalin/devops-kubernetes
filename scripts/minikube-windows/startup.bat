e68d514e9fde83b974f03ae9bc788a7506fd291d
Kunal Kapoor <kunalkapoor.com@gmail.com>
2016-11-22 09:06:26 -0500

    Changed to bat. Added command minikube start

--- scripts/minikube-windows/startup.bat
+++ scripts/minikube-windows/startup.bat
@@ -6,3 +6,6 @@
 # Install Minikube from https://github.com/kubernetes/minikube/releases
 
 # Install Kubectl from http://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/windows/amd64/kubectl.exe
+
+# Create a local Kubernetes cluster with one node using Minikube
+minikube start
